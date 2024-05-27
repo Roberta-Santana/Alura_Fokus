3ba25276ea2d0ba829a2e16ecab649613f4539f7
Roberta Santana <g.roberta.s@hotmail.com>
2024-05-27 18:36:06 -0300

    Alterando texto com switch e innerHTML; foreach para add-remove class;

--- script.js
+++ script.js
@@ -2,28 +2,54 @@ const html = document.querySelector('html');
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBT = document.querySelector('.app__card-button--longo');
+const botoes = document.querySelectorAll(".app__card-button");
 
 const telaTempo = document.querySelector('#timer')//getElementById('timer');
 const imagem = document.querySelector('.app__image')//getElementsByClassName('app__image');
-const texto = document.getElementsByClassName('app__title');
+const texto = document.querySelector('.app__title'); 
+//não sei pq, getbyClassName não funcionou no innerHTML
 
 const startButton = document.querySelector('.app__card-primary-button')//getElementById('start-pause');
 const focoTempo = 1500;
 const curtoTempo = 300;
 const longoTempo = 900;
 
+function alterarContexto(contexto){
+    botoes.forEach(function (contexto){
+        contexto.classList.remove('active');
+    })
+    html.setAttribute('data-contexto', contexto);
+    imagem.setAttribute('src', `./imagens/${contexto}.png`);
+    switch (contexto) {
+    case "foco":
+            texto.innerHTML= `Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            break;
+    case "descanso-curto":
+           texto.innerHTML = `Que tal dar uma respirada<br>
+                <strong class="app__title-strong">Faça uma pausa.</strong>`
+            break;
+    case 'descanso-longo':
+            texto.innerHTML= `Hora de voltar a superfície<br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+            break;    
+        default:
+            break;
+    }
+}
+
 focoBt.addEventListener('click', ()=>{
-    html.setAttribute('data-contexto', 'foco');
-    imagem.setAttribute('src','/imagens/foco.png');
+    alterarContexto('foco');
+    focoBt.classList.add('active');
 })
 
 curtoBt.addEventListener('click',()=>{
-    html.setAttribute('data-contexto', 'descanso-curto');
-    imagem.setAttribute('src','/imagens/descanso-curto.png');
+    alterarContexto('descanso-curto');
+    curtoBt.classList.add('active');
 })
 
 longoBT.addEventListener('click', ()=>{
-    html.setAttribute('data-contexto', 'descanso-longo');
-    imagem.setAttribute('src','/imagens/descanso-longo.png')
+    alterarContexto('descanso-longo');
+    longoBT.classList.add('active');
 })
 
