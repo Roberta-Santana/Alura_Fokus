f0ae89bfce83143e335e2576613b7492935981e8
Roberta Santana <g.roberta.s@hotmail.com>
2024-06-02 15:04:33 -0300

    temporizador terminado

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const botoes = document.querySelectorAll(".app__card-button");
 const musicaFocoInput = document.querySelector('#alternar-musica');
 const musica = new Audio('/sons/luna-rise-part-one.mp3');
 musica.loop = true;
-
+const btTexto = document.querySelector('#start-pause span');
+const btImg = document.querySelector('.app__card-primary-butto-icon');
 
 const telaTempo = document.querySelector('#timer')//getElementById('timer');
 const imagem = document.querySelector('.app__image')//getElementsByClassName('app__image');
@@ -14,14 +15,15 @@ const texto = document.querySelector('.app__title');
 //não sei pq, getbyClassName não funcionou no innerHTML
 const somPause = new Audio('/sons/pause.mp3');
 const somComecar = new Audio('/sons/play.wav');
-
-let tempoDecorridoEmSegundos = 5;
-let intervaloId= null;
+const somFinal = new Audio('/sons/beep.mp3');
 
 const startButton = document.querySelector('.app__card-primary-button')//getElementById('start-pause');
 const focoTempo = 1500;
 const curtoTempo = 300;
 const longoTempo = 900;
+
+let tempoDecorridoEmSegundos=1500;
+let intervaloId= null;
 
 function alterarContexto(contexto){
     botoes.forEach(function (contexto){
@@ -31,33 +33,40 @@ function alterarContexto(contexto){
     imagem.setAttribute('src', `./imagens/${contexto}.png`);
     switch (contexto) {
     case "foco":
+        //tempoDecorridoEmSegundos = focoTempo;
             texto.innerHTML= `Otimize sua produtividade,<br>
                 <strong class="app__title-strong">mergulhe no que importa.</strong>`
             break;
     case "descanso-curto":
+        //tempoDecorridoEmSegundos = curtoTempo;
            texto.innerHTML = `Que tal dar uma respirada<br>
                 <strong class="app__title-strong">Faça uma pausa.</strong>`
             break;
     case 'descanso-longo':
+        //tempoDecorridoEmSegundos=longoTempo;
             texto.innerHTML= `Hora de voltar a superfície<br>
                 <strong class="app__title-strong">Faça uma pausa longa.</strong>`
             break;    
         default:
             break;
     }
+    mostraTempo();
 }
 
 focoBt.addEventListener('click', ()=>{
+    tempoDecorridoEmSegundos = focoTempo;
     alterarContexto('foco');
     focoBt.classList.add('active');
 })
 
 curtoBt.addEventListener('click',()=>{
+    tempoDecorridoEmSegundos = curtoTempo;
     alterarContexto('descanso-curto');
     curtoBt.classList.add('active');
 })
 
 longoBT.addEventListener('click', ()=>{
+    tempoDecorridoEmSegundos=longoTempo;
     alterarContexto('descanso-longo');
     longoBT.classList.add('active');
 })
@@ -73,14 +82,13 @@ musicaFocoInput.addEventListener('change', ()=>{
 const contagemRegressiva = ()=>{ //arow function
     //inicar();
     if(tempoDecorridoEmSegundos<=0){
-        zerar();
+        //somFinal.play();
         alert('Tempo finalizado!');
-        console.log(intervaloId);
-
+        zerar();
         return        
     }
-        tempoDecorridoEmSegundos --;
-        console.log('Temporizador: '+tempoDecorridoEmSegundos);
+    tempoDecorridoEmSegundos --;
+    mostraTempo();
 }
 
 startButton.addEventListener('click', inicar)
@@ -88,16 +96,28 @@ startButton.addEventListener('click', inicar)
 function inicar(){
     if(intervaloId){
         somPause.play();
+        btImg.setAttribute('src', './imagens/play_arrow.png');
         zerar();
         console.log(intervaloId);
     return
     }
     somComecar.play();
-    intervaloId=setInterval(contagemRegressiva,1000)
+    btImg.setAttribute('src', './imagens/pause.png');
+    intervaloId=setInterval(contagemRegressiva,1000);
+    btTexto.textContent='Pausar';
     console.log(intervaloId);
 }
 
 function zerar(){
     clearInterval(intervaloId);
+    btTexto.textContent='Vamos lá!';
     intervaloId=null;
 }
+
+function mostraTempo(){
+    const tempo = new Date(tempoDecorridoEmSegundos*1000);
+    const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute:'2-digit', second:'2-digit'});
+    telaTempo.innerHTML = `${tempoFormatado}`
+    console.log(tempoFormatado);
+}
+mostraTempo();
